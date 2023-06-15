e8e8c4f1c205a937483b0f174aaf392dbda0627a
wynn5a <winminy@163.com>
2023-06-15 13:36:49 +0800

    clean dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,17 +13,14 @@ repositories {
     mavenCentral()
 }
 dependencies {
-//    implementation("org.redisson:redisson:3.22.0")
     implementation("org.redisson:redisson-spring-boot-starter:3.22.0")
     runtimeOnly("io.netty:netty-resolver-dns-native-macos:4.1.93.Final:osx-aarch_64")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0")
     implementation("org.apache.logging.log4j:log4j-api:2.20.0")
     implementation("org.apache.logging.log4j:log4j-core:2.20.0")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(platform("org.junit:junit-bom:5.9.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("io.projectreactor:reactor-test:3.5.6")
 }
 
 tasks.test {
